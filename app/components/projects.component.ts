1a5284e78cb07ce8da6528e809fd56a1a4ccdfec
Dylan Gattey <dylan.gattey@gmail.com>
2016-04-04 01:59:45 -0400

    Got rid of extraneous p around excerpt using pipes

--- app/components/projects.component.ts
+++ app/components/projects.component.ts
@@ -1,13 +1,24 @@
-import {Component, OnInit} from 'angular2/core';
+import {Component, OnInit, Pipe, PipeTransform} from 'angular2/core';
 import {Router } from 'angular2/router';
 
 import {Project} from '../models/project.model';
 import {WPService} from '../services/wp.service';
 
+@Pipe({name: 'escapeP', pure: false})
+class EscapePPipe implements PipeTransform {
+	transform(value: any, args: any[] = []) {
+		// Remove surrounding <p> tags
+		if(value.indexOf('<p>') != -1) {
+			return value.replace('<p>', '').replace('<\/p>', '');
+		}
+	}
+}
+
 @Component({
 	selector: 'projects',
 	templateUrl: 'app/static/projects.component.html',
-	styleUrls: ['app/styles/projects.component.css']
+	styleUrls: ['app/styles/projects.component.css'],
+	pipes: [EscapePPipe]
 })
 export class ProjectsComponent implements OnInit {
 	projects: Array<Project>;
