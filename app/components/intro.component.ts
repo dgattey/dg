a60b1e5578fe5b46708acd2f9be1eec90b835199
Dylan Gattey <dylan.gattey@gmail.com>
2016-04-05 02:00:18 -0400

    Separation of scss to files, intro styles, more variables

--- app/components/intro.component.ts
+++ app/components/intro.component.ts
@@ -3,8 +3,7 @@ import { Component } from 'angular2/core';
 @Component({
 	selector: 'intro-area',
 	templateUrl: 'app/static/intro.component.html'
+	//TODO: Not working... styleUrls: ['app/styles/intro.component.css']
 })
-export class IntroComponent {
-
-}
+export class IntroComponent {}
 
