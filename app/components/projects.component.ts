a76b6453ec1ef9126610c5a1a319007eb33728e8
Dylan Gattey <dylan.gattey@gmail.com>
2016-04-07 01:28:32 -0400

    Project card hover responsive with vw, new selectors

--- app/components/projects.component.ts
+++ app/components/projects.component.ts
@@ -3,26 +3,7 @@ import {Router } from 'angular2/router';
 
 import {Project} from '../models/project.model';
 import {WPService} from '../services/wp.service';
-
-@Pipe({name: 'escapeP', pure: false})
-class EscapePPipe implements PipeTransform {
-	transform(value: any, args: any[] = []) {
-		// Remove surrounding <p> tags
-		if(value.indexOf('<p>') != -1) {
-			return value.replace('<p>', '').replace('<\/p>', '');
-		}
-	}
-}
-
-@Component({
-	selector: 'project-card',
-	templateUrl: 'app/static/projectCard.component.html',
-	inputs: ['model'],
-	pipes: [EscapePPipe]
-})
-export class ProjectCardComponent {
-	model: Project;
-}
+import {ProjectCardComponent} from './project-card.component';
 
 @Component({
 	selector: 'projects',
@@ -46,5 +27,9 @@ export class ProjectsComponent implements OnInit {
 			}
 			console.log('Loaded Projects!', this.projects);
 		});
+	}
+
+	highlight(project: Project) {
+		console.log('Highlighted project!', project);
 	}
 }
