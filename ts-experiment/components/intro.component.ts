9804d322cec7cabe3ec4212accfc0797d8665c26
Dylan Gattey <dylan.gattey@gmail.com>
2016-04-28 23:45:18 -0400

    Deleting old content

--- ts-experiment/components/intro.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from 'angular2/core';
-
-@Component({
-	selector: 'intro-area',
-	templateUrl: 'app/static/intro.component.html'
-	//TODO: Not working... styleUrls: ['app/styles/intro.component.css']
-})
-export class IntroComponent {}
-
