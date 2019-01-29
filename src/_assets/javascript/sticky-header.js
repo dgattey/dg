ede8528c9a63c57f9b4ec909dbd05ba3fea25a07
Dylan Gattey <dgattey@linkedin.com>
2019-01-29 00:01:49 -0800

    Also add sticky header class on load

--- src/_assets/javascript/sticky-header.js
+++ src/_assets/javascript/sticky-header.js
@@ -2,11 +2,13 @@ const header = document.querySelector(".header-nav")
 const headerHeight = header.offsetHeight
 const scrolledClass = "shadow-weak"
 
-// On scroll, add the class specified
-window.addEventListener('scroll', function() {
-  if (window.scrollY >= headerHeight) {
-    header.classList.add(scrolledClass)
-  } else {
-    header.classList.remove(scrolledClass)
-  }
-})
+var addOrRemoveClass = function() {
+	if (window.scrollY >= headerHeight) {
+		header.classList.add(scrolledClass)
+	} else {
+		header.classList.remove(scrolledClass)
+	}
+}
+// On scroll and load, add the class specified
+window.addEventListener('scroll', addOrRemoveClass());
+window.addEventListener('load', addOrRemoveClass());
