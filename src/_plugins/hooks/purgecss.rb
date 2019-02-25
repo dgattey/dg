422a4ca9dc54fbfa3ce1e90648c7d247469c1c8a
Dylan Gattey <hi@dylangattey.com>
2019-02-24 20:43:46 -0800

    Overhauls links, nav markup, adds more grid options

--- src/_plugins/hooks/purgecss.rb
+++ src/_plugins/hooks/purgecss.rb
@@ -18,7 +18,7 @@ Jekyll::Hooks.register(:site, :post_write) do |_site|
       content: [@build_directory + '/**/*.html'],
       css: [Dir.glob(@build_directory + '/assets/*.css').first],
       whitelist: %w[overhang-parent-top-4-tablet overhang-parent-bottom-4-tablet
-                    flush-left-4 round-corners
+                    flush-left-4 round-corners underline-on-hover link-no-hover
                     shadow-weak wf-active wf-inactive]
     }.stringify_keys.to_json}"''
 
