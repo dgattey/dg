99924ae0c7c04ab11911b0ad12d0ddbb91dc9437
Dylan Gattey <dylan.gattey@gmail.com>
2021-11-20 01:14:21 -0800

    feat: add next js + swap configs to .js

--- gatsby-config.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// Reads TS config automatically and use the ts version of the config
-const { generateConfig } = require('gatsby-plugin-ts-config')
-module.exports = generateConfig({
-	configDir: 'config',
-})
---
+++ next.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ * */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+};
+
+module.exports = nextConfig;
