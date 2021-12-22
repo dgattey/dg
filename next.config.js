1b668ccaf89f92dad63d1b14d95e435f9c385f1e
Dylan Gattey <dylan.gattey@gmail.com>
2021-12-22 01:29:18 -0800

    fix: uses version matching commit instead of latest

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@
  * */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      const copy = { ...config };
+      copy.resolve.fallback = { fs: false, path: false, child_process: false, util: false };
+      return copy;
+    }
+    return config;
+  },
   experimental: {
     // Enables the styled-components SWC transform
     styledComponents: true,
--- src/api/github/fetchRepoVersion.ts
+++ src/api/github/fetchRepoVersion.ts
@@ -5,19 +5,37 @@ import { DgRepoLatestReleaseQuery } from './generated/fetchRepoVersion.generated
 const QUERY = gql`
   query DgRepoLatestRelease {
     repository(name: "dg", owner: "dgattey") {
-      latestRelease {
-        name
+      releases(last: 100) {
+        nodes {
+          name
+          tagCommit {
+            oid
+          }
+        }
       }
     }
   }
 `;
 
 /**
- * Grabs latest version of the repo from Github
+ * Grabs the last 100 versions from Github, and our HEAD commit SHA from
+ * the filesystem. Compare the releases' `oid`s to the current HEAD to
+ * see which one matches. If any do, the first is returned. Won't be able
+ * to run on client, but it'll gracefully fallback to the fallback.
  */
 const fetchRepoVersion = async () => {
   const data = await fetchGithubData<DgRepoLatestReleaseQuery>(QUERY);
-  return data?.repository?.latestRelease?.name;
+  const releases = data?.repository?.releases?.nodes;
+  try {
+    const { promisify } = await import('util');
+    const exec = promisify((await import('child_process')).exec);
+    const { stdout: commitSha } = await exec('git rev-parse HEAD');
+    const filteredReleases =
+      releases?.filter((release) => release?.tagCommit?.oid === commitSha.trim()) ?? [];
+    return filteredReleases[0]?.name ?? null;
+  } catch {
+    return undefined;
+  }
 };
 
 export default fetchRepoVersion;
