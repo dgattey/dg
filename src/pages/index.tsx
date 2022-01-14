df78659bbb2e77ca024bab409956bd0fe32ae51b
Dylan Gattey <dylan.gattey@gmail.com>
2022-01-13 19:09:54 -0800

    chore: separate data + fallback for better rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
-import useData, { Fallback, fetchData } from 'api/useData';
-import ContentGrid from 'components/ContentGrid';
+import { Fallback, fetchData } from 'api/useData';
 import Layout from 'components/Layout';
 import Meta from 'components/Meta';
-import ProjectCard from 'components/ProjectCard';
+import ProjectGrid from 'components/ProjectGrid';
 import { GetStaticProps, InferGetStaticPropsType } from 'next/types';
 import React from 'react';
 import { SWRConfig } from 'swr';
@@ -40,23 +39,16 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 /**
  * Homepage, shows projects + other cards in a grid
  */
-const Home = ({ fallback }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const { data: projects } = useData('projects');
-  return (
-    <SWRConfig value={{ fallback }}>
-      <Meta
-        title="Engineer, Human"
-        description="I'm Dylan, an engineer focused on building top-notch user experiences. I'm interested in React, Product Design, Sustainability, Startups, Music, and Cycling."
-      />
-      <Layout>
-        <ContentGrid>
-          {projects?.map((project) => (
-            <ProjectCard key={project.title} {...project} />
-          ))}
-        </ContentGrid>
-      </Layout>
-    </SWRConfig>
-  );
-};
+const Home = ({ fallback }: InferGetStaticPropsType<typeof getStaticProps>) => (
+  <SWRConfig value={{ fallback }}>
+    <Meta
+      title="Engineer, Human"
+      description="I'm Dylan, an engineer focused on building top-notch user experiences. I'm interested in React, Product Design, Sustainability, Startups, Music, and Cycling."
+    />
+    <Layout>
+      <ProjectGrid />
+    </Layout>
+  </SWRConfig>
+);
 
 export default Home;
