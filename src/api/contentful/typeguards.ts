775c712186a0e4dabd6a9c61d32dca416b97b3f8
Dylan Gattey <dylan.gattey@gmail.com>
2022-01-13 23:51:08 -0800

    feat: support for RichText

--- src/api/contentful/typeguards.ts
+++ src/api/contentful/typeguards.ts
@@ -1,4 +1,4 @@
-import type { Link, Project } from './generated/api.generated';
+import type { Link, Project, TextBlock } from './generated/api.generated';
 
 /**
  * Type guard to get a link out
@@ -13,6 +13,14 @@ export const isLink = (item: Link | undefined | Record<string, unknown>): item i
 export const isProject = (item: Project | undefined | Record<string, unknown>): item is Project =>
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
   !!(item as Project)?.creationDate && !!(item as Project)?.thumbnail;
+
+/**
+ * Type guard to get a text block out
+ */
+export const isTextBlock = (
+  item: TextBlock | undefined | Record<string, unknown>,
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+): item is TextBlock => !!(item as TextBlock)?.content?.json;
 
 /**
  * Type guard to filter out null or undefined items
