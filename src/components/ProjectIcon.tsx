06a1732a902164a5a74b746ac8ff41e83629dee7
Dylan Gattey <dylan.gattey@gmail.com>
2022-01-13 19:01:04 -0800

    feat: fancy sliding titles + icons on projects

---
+++ src/components/ProjectIcon.tsx
@@ -0,0 +1,28 @@
+import { Project } from 'api/contentful/generated/api.generated';
+import { FiGlobe, FiMonitor, FiSmartphone } from 'react-icons/fi';
+
+type Props = Pick<Project, 'type'>;
+
+/**
+ * Returns a Feather icon matching a project type or null
+ */
+const ProjectIcon = ({
+  type,
+  className,
+}: Props & Pick<React.ComponentProps<'div'>, 'className'>) => {
+  if (!type?.[0]) {
+    throw new TypeError('Invalid type for project');
+  }
+  switch (type[0]) {
+    case 'Website':
+      return <FiGlobe className={className} />;
+    case 'iOS App':
+      return <FiSmartphone className={className} />;
+    case 'macOS App':
+      return <FiMonitor className={className} />;
+    default:
+      return null;
+  }
+};
+
+export default ProjectIcon;
