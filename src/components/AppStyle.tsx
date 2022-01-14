f092322345707adeaa35203d980bd6affc98dc9e
Dylan Gattey <dylan.gattey@gmail.com>
2022-01-13 19:00:46 -0800

    chore: dark/light mode specific CSS variables

--- src/components/AppStyle.tsx
+++ src/components/AppStyle.tsx
@@ -1,5 +1,5 @@
 import '@picocss/pico/css/pico.min.css';
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 const CONTENT_GRID_DIMENSION = 17;
 const CONTENT_GRID_GAP = 2.5;
@@ -9,6 +9,24 @@ const CONTENT_GRID_GAP = 2.5;
  */
 export const cardSizeInEm = (span = 1) =>
   CONTENT_GRID_DIMENSION * span + (span - 1) * CONTENT_GRID_GAP;
+
+/**
+ * Variables specific to dark mode. Anything that appears here needs to have
+ * something defined in `lightModeVariables` and vice versa.
+ */
+const darkModeVariables = css`
+  --contrast-overlay: var(--contrast-inverse);
+  --contrast-overlay-inverse: var(--contrast);
+`;
+
+/**
+ * Variables specific to light mode. Anything that appears here needs to have
+ * something defined in `darkModeVariables` and vice versa.
+ */
+const lightModeVariables = css`
+  --contrast-overlay: var(--secondary);
+  --contrast-overlay-inverse: var(--secondary-inverse);
+`;
 
 /**
  * Applies styles to the full app
@@ -21,6 +39,24 @@ const AppStyle = createGlobalStyle`
       /* Below here are new variables */
       --content-grid-dimension-em: ${CONTENT_GRID_DIMENSION}em;
       --content-grid-gap-em: ${CONTENT_GRID_GAP}em;
+  }
+
+  // When a page/component is set to light or the root has no dark theme applied
+  [data-theme='light'],
+  :root:not([data-theme='dark']) {
+    ${lightModeVariables};
+  }
+
+  // When a page/component is set to dark
+  [data-theme='dark'] {
+    ${darkModeVariables};
+  }
+
+  // When dark mode is on and the root isn't set to light
+  @media only screen and (prefers-color-scheme: dark) {
+    :root:not([data-theme='light']) {
+      ${darkModeVariables};
+    }
   }
 `;
 
