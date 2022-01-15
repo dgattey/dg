42667f707b40ba1608dabd1ca16f193756a771ae
Dylan Gattey <dylan.gattey@gmail.com>
2022-01-15 02:02:31 -0800

    feat: uses Inter for logo + fun sticky header blobby scroll to top

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,23 @@
+import styled from 'styled-components';
 import Footer from './Footer';
 import Header from './Header';
+
+const HeaderBar = styled.div`
+  position: sticky;
+  top: 0;
+  z-index: 1;
+`;
 
 /**
  * Creates a basic page layout. Should wrap every page!
  */
 const Layout = ({ children }: Pick<React.ComponentProps<'div'>, 'children'>) => (
   <>
-    <section className="container">
-      <Header />
-    </section>
+    <HeaderBar>
+      <section className="container">
+        <Header />
+      </section>
+    </HeaderBar>
     <section className="container">
       <main>{children}</main>
     </section>
