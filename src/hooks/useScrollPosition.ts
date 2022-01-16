e56cc51012555f646786cf2e663b8c8e71bed30a
Dylan Gattey <dylan.gattey@gmail.com>
2022-01-15 16:15:48 -0800

    fix: throttle the scroll position handler

--- src/hooks/useScrollPosition.ts
+++ src/hooks/useScrollPosition.ts
@@ -1,34 +1,28 @@
-/**
- * useScroll React custom hook
- * Usage:
- *    const { scrollX, scrollY, scrollDirection } = useScroll();
- */
-
+import { useThrottleCallback } from '@react-hook/throttle';
 import { useEffect, useState } from 'react';
 
 /**
- * Grabs the document's scroll X and Y
+ * Reports the document's X and Y offset, with a delay
  */
 const useScrollPosition = () => {
-  const [bodyOffset, setBodyOffset] = useState<DOMRect | null>(null);
-  const [scrollY, setScrollY] = useState(bodyOffset?.top);
-  const [scrollX, setScrollX] = useState(bodyOffset?.left);
+  const [scrollY, setScrollY] = useState(0);
+  const [scrollX, setScrollX] = useState(0);
 
-  const updateOffset = () =>
-    requestAnimationFrame(() => {
-      const rect = document.body.getBoundingClientRect();
-      setBodyOffset(rect);
-      setScrollY(-rect.top);
-      setScrollX(rect.left);
-    });
+  // Updates the offset with a resolution of 15 FPS for best performance
+  const updateOffset = useThrottleCallback(() => {
+    const rect = document.body.getBoundingClientRect();
+    setScrollY(-rect.top);
+    setScrollX(rect.left);
+  }, 15);
 
+  // Attach to the window's scroll listener
   useEffect(() => {
     updateOffset();
     window.addEventListener('scroll', updateOffset);
     return () => {
       window.removeEventListener('scroll', updateOffset);
     };
-  }, []);
+  }, [updateOffset]);
 
   return {
     scrollY,
