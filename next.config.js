5c13e10c0b35eac649eac2d191c06f02ee250cd2
Dylan Gattey <dylan.gattey@gmail.com>
2022-01-17 23:55:57 -0800

    fix: get next working again

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
   experimental: {
     // Enables the styled-components SWC transform
     styledComponents: true,
+
+    // Allows Yarn PnP to work right now
+    swcFileReading: false,
   },
   images: {
     domains: ['images.ctfassets.net'],
