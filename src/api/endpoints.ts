dab525f6bdb40e6f30d0e80870f6c93d87db8686
Dylan Gattey <dylan.gattey@gmail.com>
2022-01-26 23:54:11 -0800

    chore: swap to non-dynamic imports as I can verify it doesn't double import

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,3 +1,10 @@
+import fetchContentfulFooterLinks from './server/fetchContentfulFooterLinks';
+import fetchContentfulHeaderLinks from './server/fetchContentfulHeaderLinks';
+import fetchContentfulIntroBlock from './server/fetchContentfulIntroBlock';
+import fetchContentfulLocation from './server/fetchContentfulLocation';
+import fetchContentfulProjects from './server/fetchContentfulProjects';
+import fetchGithubRepoVersion from './server/fetchGithubRepoVersion';
+
 /**
  * All possible types of endpoints we could request
  */
@@ -16,31 +23,27 @@ export type AwaitedType<Key extends EndpointKey> = Awaited<EndpointType<Key>>;
 /**
  * These represent all possible endpoints our server can handle/call. To add
  * a new one, add an import for the fetcher and add a short key to identify it.
- * The keys can have slashes, representing sub paths.
- *
- * IMPORTANT: every one of these should be dynamic imports as this file is used
- * on both the server and client sides and needs to be safe for both. Unfortunately
- * that means that you have to await the import and then call the default function,
- * but it keeps the API elsewhere clean.
+ * The keys can have slashes, representing sub paths. Use this ONLY from the
+ * server or `getStaticProps`, NOT from the client!
  */
 const endpoints = {
   // Intro block of content via Contentful
-  intro: async () => (await import('./server/fetchContentfulIntroBlock')).default(),
+  intro: fetchContentfulIntroBlock,
 
   // My current location via Contentful
-  location: async () => (await import('./server/fetchContentfulLocation')).default(),
+  location: fetchContentfulLocation,
 
   // All projects via Contentful
-  projects: async () => (await import('./server/fetchContentfulProjects')).default(),
+  projects: fetchContentfulProjects,
 
   // Footer links via Contentful
-  footer: async () => (await import('./server/fetchContentfulFooterLinks')).default(),
+  footer: fetchContentfulFooterLinks,
 
   // Header data via Contentful
-  header: async () => (await import('./server/fetchContentfulHeaderLinks')).default(),
+  header: fetchContentfulHeaderLinks,
 
   // App version via Github
-  version: async () => (await import('./server/fetchGithubRepoVersion')).default(),
+  version: fetchGithubRepoVersion,
 } as const;
 
 /**
