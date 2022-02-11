7e28d4e887812d41415bb51a1923e9f46795694b
Dylan Gattey <dylan.gattey@gmail.com>
2022-02-11 00:42:06 -0800

    fix: apply image sizing to MapMarker too

--- src/components/maps/Marker.tsx
+++ src/components/maps/Marker.tsx
@@ -1,9 +1,11 @@
 import type { MapLocation } from 'api/types/MapLocation';
+import Image from 'components/Image';
 import { Marker as MapMarker } from 'react-map-gl';
 import styled from 'styled-components';
 
 const DIMENSION = 100;
 const RADIUS = DIMENSION / 2;
+const IMAGE_DIMENSION = DIMENSION * 0.85;
 
 // Required point, optional image
 type Props = Pick<MapLocation, 'point'> & Partial<Pick<MapLocation, 'image'>>;
@@ -17,6 +19,12 @@ const AreaIndicator = styled.circle.attrs({ r: RADIUS, cx: RADIUS, cy: RADIUS })
   stroke-width: 1px;
 `;
 
+const ImageContainer = styled.span`
+  position: absolute;
+  left: ${(DIMENSION - IMAGE_DIMENSION) / 2}px;
+  top: ${(DIMENSION - IMAGE_DIMENSION) / 2}px;
+`;
+
 /**
  * Creates a standard map marker, centered on a point
  */
@@ -24,8 +32,17 @@ const Marker = ({ point, image }: Props) => (
   <MapMarker {...point}>
     <svg width={DIMENSION} height={DIMENSION}>
       <AreaIndicator />
-      {image && <image width={DIMENSION} height={DIMENSION} href={image.url} />}
     </svg>
+    {image && (
+      <ImageContainer>
+        <Image
+          width={IMAGE_DIMENSION}
+          height={IMAGE_DIMENSION}
+          url={image.url}
+          alt={`${point?.latitude} ${point?.longitude}`}
+        />
+      </ImageContainer>
+    )}
   </MapMarker>
 );
 
