ba69573e536aa0d72ff0ef80b1c07e0354871f2a
Dylan Gattey <dylan.gattey@gmail.com>
2022-05-07 01:48:46 -0700

    chore: update privacy meta title

--- src/components/privacy/PrivacyMeta.tsx
+++ src/components/privacy/PrivacyMeta.tsx
@@ -4,7 +4,7 @@ import Meta from 'components/Meta';
 /**
  * Creates metadata for the privacy page from privacy data
  */
-const HomepageMeta = () => {
+const PrivacyMeta = () => {
   const { data: privacyBlock } = useData('privacy');
 
   // Grabs the second text element, since the first is a last updated
@@ -14,7 +14,7 @@ const HomepageMeta = () => {
 
   const text = secondParagraph?.content?.map((node) => node.value)?.join('') ?? '';
 
-  return <Meta title="Engineer. Problem Solver." description={text} />;
+  return <Meta title="Privacy Policy" description={text} />;
 };
 
-export default HomepageMeta;
+export default PrivacyMeta;
