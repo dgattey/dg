43392ca3582d1f36b5b11fbb9a2c2abaf11eeac5
Dylan Gattey <dylan.gattey@gmail.com>
2022-12-02 20:50:20 -0800

    misc fixes

--- config/lint-staged.config.js
+++ config/lint-staged.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  // In the project folder we type check, lint, lint styles, and format
-  'src/**/*.{ts,tsx,js,jsx}': [
+  // In js/jsx/ts/tsx we type check, lint, lint styles, and format
+  '**/*.{ts,tsx,js,jsx}': [
     () => 'tsc',
     'eslint --cache --fix --no-eslintrc -c config/eslint.config.js',
     'prettier --loglevel warn --write',
   ],
   // Outside of the project folder or in JSON/etc we just format and call it good
-  '*.{ts,tsx,mdx,js,jsx,json,md,yml,css}': ['prettier --loglevel warn --write'],
+  '*.{mdx,json,md,yml,css}': ['prettier --loglevel warn --write'],
 };
