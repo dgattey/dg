5ce2af7327a731548f947091ecb9381825f3e0ca
Dylan Gattey <dylan.gattey@gmail.com>
2022-12-10 23:37:13 -0800

    fix: all files participate in lint/typecheck

--- config/lint-staged.config.mjs
+++ config/lint-staged.config.mjs
@@ -7,8 +7,12 @@ const lint = 'turbo lint -- ';
 const lintTypes = 'turbo lint:types';
 
 const jobs = {
-  // In main project files we type check, lint, lint styles, and format
-  '**/*.{js,ts,tsx}': (files) => {
+  /**
+   * In main project files we type check, lint, lint styles, and format
+   *
+   * @param   {Array<string>}  files  The file paths we are using
+   */
+  '**/*.{js,ts,tsx,mjs,cjs}': (files) => {
     const match = micromatch.not(files, migrationsPath);
     const filteredFiles = match.join(' ');
     return [
@@ -22,7 +26,7 @@ const jobs = {
   [migrationsPath]: format,
 
   // In non js/ts files, just format
-  '*.{js,ts,tsx,css,md,json,yml}': format,
+  '*.{js,ts,tsx,css,md,json,yml,yaml}': format,
 };
 
 export default jobs;
