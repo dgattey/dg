4bead9050c4d093799928e0f8d27558392246332
Dylan Gattey <dylan.gattey@gmail.com>
2022-12-28 15:31:30 -0800

    chore: fix the class name generator for switch maybe

--- src/ui/theme/classNameSetupOnImport.ts
+++ src/ui/theme/classNameSetupOnImport.ts
@@ -1,22 +1,6 @@
 import { unstable_ClassNameGenerator as ClassNameGenerator } from '@mui/material/className';
 
-let currentHash = 0;
-
-/**
- * Maps from a component name to its hash for lifetime of app.
- */
-const HASHES: Record<string, string> = {};
-
 /**
  * Short class names for MUI components in production, for better performance.
  */
-ClassNameGenerator.configure((componentName) => {
-  if (process.env.NODE_ENV !== 'production') {
-    return componentName.replace('Mui', '');
-  }
-  const hash = HASHES[componentName] ?? currentHash.toString(36);
-  if (!HASHES[componentName]) {
-    currentHash += 1;
-  }
-  return hash;
-});
+ClassNameGenerator.configure((componentName) => componentName.replace('Mui', ''));
