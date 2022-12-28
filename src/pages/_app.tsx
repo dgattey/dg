7d779504cafcfdfcdb05be7ed03731c539afe09f
Dylan Gattey <dylan.gattey@gmail.com>
2022-12-28 02:34:38 -0800

    chore: rename GlobalStyleProvider filename

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import 'ui/theme/classNameSetupOnImport';
 
-import { GlobalStyleProvider } from 'ui/theme/GlobalStyle';
+import { GlobalStyleProvider } from 'ui/theme/GlobalStyleProvider';
 import { AppProps } from 'next/app';
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
