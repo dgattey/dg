c1b3305ff7fa4b81d146cb5df0540bc9c043e8e0
Dylan Gattey <sighted_spur0b@icloud.com>
2023-12-30 15:56:35 -0800

    [API] use wretch library for easier fetch calls (#429)
    Closes DG-138
    
    ## What changed? Why?
    Moves to using a helper fetch library for the REST & frontend situations
    that previously used fetch directly. Similar but nicer API & such.

--- apps/web/src/api/server/spotify/fetchRecentlyPlayed.ts
+++ apps/web/src/api/server/spotify/fetchRecentlyPlayed.ts
@@ -12,25 +12,33 @@ const RECENTLY_PLAYED_RESOURCE = 'me/player/recently-played?limit=1';
  * as `null`, or returns full JSON.
  */
 export async function fetchRecentlyPlayed(): Promise<null | Track> {
-  const currentlyPlaying = await spotifyClient.fetch<CurrentlyPlaying>(CURRENTLY_PLAYING_RESOURCE);
-
-  switch (currentlyPlaying.status) {
+  const { response, status } = await spotifyClient.get(CURRENTLY_PLAYING_RESOURCE);
+  switch (status) {
     case 200: {
-      const data = await currentlyPlaying.json();
-      return data?.item ?? null;
+      const { item } = await response.json<CurrentlyPlaying>();
+      return item;
     }
     case 204: {
-      // Fetch the last played song instead
-      const recentlyPlayed = await spotifyClient.fetch<RecentlyPlayed>(RECENTLY_PLAYED_RESOURCE);
-      if (recentlyPlayed.status !== 200) {
-        return null;
-      }
-      const data = await recentlyPlayed.json();
-      const item = data?.items[0];
-      return item ? { ...item.track, played_at: item.played_at } : null;
+      // We aren't currently playing a song but we could have recently played one
+      return fetchLastPlayed();
     }
     default:
       // This could be rate limiting, or auth problems, etc.
       return null;
   }
+}
+
+/**
+ * Fetches the song that last played from spotify using a valid access token.
+ * May have no content, which signifies nothing is playing, which is returned
+ * as `null`, or returns full JSON.
+ */
+async function fetchLastPlayed(): Promise<null | Track> {
+  const { response, status } = await spotifyClient.get(RECENTLY_PLAYED_RESOURCE);
+  if (status !== 200) {
+    return null;
+  }
+  const data = await response.json<RecentlyPlayed>();
+  const item = data.items[0];
+  return item ? { ...item.track, played_at: item.played_at } : null;
 }
