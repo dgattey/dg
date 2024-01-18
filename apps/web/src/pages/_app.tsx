d7d27edab90779974ca5dade7fa84153b4e184b7
Dylan Gattey <dylan.gattey@gmail.com>
2024-01-17 22:46:59 -0800

    add analytics

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import { GlobalStyleProvider } from 'ui/theme/GlobalStyleProvider';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export type PageProps = Record<string, unknown>;
 
@@ -29,6 +30,7 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
     <GlobalStyleProvider>
       {getLayout(<Component {...pageProps} />, pageProps)}
       <SpeedInsights />
+      <Analytics />
     </GlobalStyleProvider>
   );
 }
