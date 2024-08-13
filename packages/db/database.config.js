c3930a162deb7b0fad9d6149f6325a490f010fa6
Dylan Gattey <dylan.gattey@gmail.com>
2024-08-13 00:32:15 -0700

    [Upgrades] all the safe ones

--- packages/db/database.config.js
+++ packages/db/database.config.js
@@ -1,8 +1,10 @@
 require('dotenv-mono').config();
+const postgres = require('pg');
 
 const SHARED_OPTIONS = {
   use_env_variable: 'DATABASE_URL',
   dialect: 'postgres',
+  dialectModule: postgres, // gets around a Vercel bug where it's missing on edge functions
   dialectOptions: {
     ssl: {
       require: true,
