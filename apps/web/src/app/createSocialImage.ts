9b108c662e2a18176771acbf9f5a383931ac7011
Dylan Gattey <dylan.gattey@watershedclimate.com>
2026-01-31 23:31:40 -0800

    images

--- apps/web/src/app/createSocialImage.ts
+++ apps/web/src/app/createSocialImage.ts
@@ -1,14 +1,11 @@
 import { generateOpenGraphImage } from '@dg/og/generateOpenGraphImage';
-import { LOGO_FONT_URL, TEXT_FONT_URL } from '@dg/og/ogFonts';
+import { getLogoFontData, getTextFontData } from '@dg/og/ogFonts';
 import { metadataBase } from './metadata';
 
 export const SOCIAL_IMAGE_SIZE = {
   height: 630,
   width: 1200,
 };
-
-const normalFont = fetch(TEXT_FONT_URL).then((res) => res.arrayBuffer());
-const boldFont = fetch(LOGO_FONT_URL).then((res) => res.arrayBuffer());
 
 /**
  * Creates a social media image (Open Graph or Twitter) for the given text.
@@ -21,8 +18,8 @@ export function createSocialImage(pathname: string, text: string, subtitle: stri
   url.searchParams.set('text', text);
   url.searchParams.set('subtitle', subtitle);
   return generateOpenGraphImage({
-    boldFont,
-    normalFont,
+    boldFont: getLogoFontData(),
+    normalFont: getTextFontData(),
     size: SOCIAL_IMAGE_SIZE,
     url: url.toString(),
   });
--- packages/og/ogFonts.ts
+++ packages/og/ogFonts.ts
@@ -3,3 +3,22 @@ export const LOGO_FONT = 'SF Pro Display Heavy';
 
 export const TEXT_FONT_URL = new URL('./SFProText.ttf', import.meta.url);
 export const LOGO_FONT_URL = new URL('./SFProDisplayHeavy.ttf', import.meta.url);
+
+let textFontData: Promise<ArrayBuffer> | undefined;
+let logoFontData: Promise<ArrayBuffer> | undefined;
+
+const loadFont = (fontUrl: URL) => fetch(fontUrl).then((res) => res.arrayBuffer());
+
+export const getTextFontData = () => {
+  if (!textFontData) {
+    textFontData = loadFont(TEXT_FONT_URL);
+  }
+  return textFontData;
+};
+
+export const getLogoFontData = () => {
+  if (!logoFontData) {
+    logoFontData = loadFont(LOGO_FONT_URL);
+  }
+  return logoFontData;
+};
