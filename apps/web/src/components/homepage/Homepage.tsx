5ddffe2534ddc70f89a82a38cac3d3fdb8b4f8c2
Dylan Gattey <dylan.gattey@gmail.com>
2026-02-06 13:05:24 -0800

    Fix cards placement code

--- apps/web/src/components/homepage/Homepage.tsx
+++ apps/web/src/components/homepage/Homepage.tsx
@@ -7,6 +7,21 @@ import { SpotifyCardSlot } from './SpotifyCardSlot';
 import { StravaCardSlot } from './StravaCardSlot';
 
 /**
+ * Merges the projects and other cards into a single array, where the other cards
+ * are interleaved between the project cards at the given indices.
+ */
+function mergeCards(
+  projects: Array<React.ReactNode>,
+  preciselyPlacedCards: Map<number, React.ReactNode>,
+): Array<React.ReactNode> {
+  const projectsIterator = projects.values();
+  return Array.from(
+    { length: projects.length + preciselyPlacedCards.size },
+    (_, i) => preciselyPlacedCards.get(i) ?? projectsIterator.next().value,
+  );
+}
+
+/**
  * Puts all projects into a grid using `projects` data,
  * interspersed with `introBlock` data, and dark/light mode
  * toggle.
@@ -15,26 +30,14 @@ export async function Homepage() {
   const projects = await getProjects();
   const projectCards = projects.map((project) => <ProjectCard key={project.title} {...project} />);
 
-  // These index into projectCards to splice in other cards
-  const otherCards = [
-    {
-      card: <IntroCardSlot key="intro" />,
-      index: 0,
-    },
-    { card: <MapPreviewCardSlot key="map" />, index: 0 },
-    {
-      card: <SpotifyCardSlot key="spotify" />,
-      index: 1,
-    },
-    { card: <StravaCardSlot key="strava" />, index: 1 },
-  ];
+  // These cards are interleaved between the project cards at the given indices. Project cards
+  // should maintain their original order, but not necessarily index.
+  const preciselyPlacedCards = new Map([
+    [0, <IntroCardSlot key="intro" />],
+    [1, <MapPreviewCardSlot key="map" />],
+    [3, <SpotifyCardSlot key="spotify" />],
+    [4, <StravaCardSlot key="strava" />],
+  ]);
 
-  return (
-    <ContentGrid>
-      {otherCards.map(({ index, card }, arrayIndex) => {
-        const nextItem = otherCards[arrayIndex + 1];
-        return [card, ...projectCards.slice(index, nextItem?.index)];
-      })}
-    </ContentGrid>
-  );
+  return <ContentGrid>{mergeCards(projectCards, preciselyPlacedCards)}</ContentGrid>;
 }
